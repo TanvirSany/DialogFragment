7fd240d7d1c8ebec4564d834dee2df08d2a56ab0
TanvirSany <tanvirrahmansany@gmail.com>
2024-09-15 17:46:45 +0600

    Design complete

---
+++ app/src/main/java/com/example/dialogfragment/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.dialogfragment
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var show: Button
+    lateinit var name: TextView
+    lateinit var age: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        show = findViewById(R.id.buttonShow)
+        name = findViewById(R.id.textViewName)
+        age = findViewById(R.id.textViewAge)
+
+        show.setOnClickListener{
+
+
+        }
+
+    }
+}
